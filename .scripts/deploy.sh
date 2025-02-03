f3fe63d417059226431565e566d4794c101a071c
koryoesz <koryoesz@gmail.com>
2025-02-03 23:51:06 +0100

    modify workflow

---
+++ .scripts/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Pull the latest version of the app
+git pull origin main
+
+# Install composer dependencies
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Clear the old cache
+php artisan clear-compiled
+
+# Recreate cache
+php artisan optimize
+
+# Run database migrations
+php artisan migrate --force
+
+echo "Deployment finished!"
